46d51d2867bb5b6dbb50155907ee1fa459af4b62
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-10 14:05:21 +0530

    First jenkins update

--- src/main/OSI.java
+++ src/main/OSI.java
@@ -3,5 +3,6 @@ package main;
 public class OSI {
     public static void main(String[] args) {
         System.out.println("OSI is my favorite company!!");
+        System.out.println("This is my first job in Jenkins");
     }
 }
