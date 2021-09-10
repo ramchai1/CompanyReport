c8a18a37e91c737dea6682279344d4cb9301e6ac
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-10 14:10:31 +0530

    Second jenkins update

--- src/main/OSI.java
+++ src/main/OSI.java
@@ -4,5 +4,6 @@ public class OSI {
     public static void main(String[] args) {
         System.out.println("OSI is my favorite company!!");
         System.out.println("This is my first job in Jenkins");
+        System.out.println("Jenkins is intelligent app for Devops");
     }
 }
