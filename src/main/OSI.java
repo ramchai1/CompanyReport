ce6723c2c24bf2b98d4ca01bb1feba549fdc876d
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-13 12:21:08 +0530

    fourth jenkins push

--- src/main/OSI.java
+++ src/main/OSI.java
@@ -6,5 +6,6 @@ public class OSI {
         System.out.println("This is my first job in Jenkins");
         System.out.println("Jenkins is intelligent app for Devops");
         System.out.println("Welcome to Developer world");
+        System.out.println("Welcome to java world");
     }
 }
